ab332fe186cd8c04d8aa86630f4355355c3a4f59
HIMANSHU <135032966+HIMANSHUDIKTIYA1@users.noreply.github.com>
2024-06-10 11:12:08 +0530

    Add files via upload

---
+++ client.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const socket = io(`http://localhost:${process.env.PORT || 8000}`);
+const form = document.getElementById('send-container');
+const MessageInput = document.getElementById('messageInp');
+const messageContainer = document.querySelector(".container");
+var audio = new Audio('ram.mp3')
+const append = (message, position)=>{
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+   
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    if(position=='left'){
+      audio.play();
+    }
+  
+}
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const message= MessageInput.value;
+    append(`you: ${message}`, `right`);
+    socket.emit(`send`, message);
+    messageInput.value = ''
+
+})
+const name = prompt("Enter your name to join ");
+socket.emit('new-user-joined', name)
+
+socket.on('user-joined', name => {
+append(`${name} joined the chat`, 'right');
+  });
+  socket.on('receive', data =>{
+    append(`${data.name}: ${data.message}`, `left`)
+  })
+  socket.on('left', name=>{
+    append(`${name} left the chat`, `left`)
+  })
